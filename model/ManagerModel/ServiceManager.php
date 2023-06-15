ea303c09a69066095773daf2a256f342748b3901
Jenniferboo <bugara.jennifer@gmail.com>
2023-06-15 10:40:46 +0200

    check

--- model/ManagerModel/ServiceManager.php
+++ model/ManagerModel/ServiceManager.php
@@ -1,6 +1,7 @@
 <?php
 namespace model\ManagerModel;
-use model\AbstractModel\MappingAbstract;
+
+use model\MappingModel\serviceMapping;
 use model\InterfaceModel\ManagerInterface;
 use PDO;
 use Exception;
@@ -8,37 +9,40 @@ use Exception;
 class ServiceManager implements ManagerInterface
 {
 
-    private PDO $pdo;
-
-
+private PDO $pdo;
 
 public function __construct(PDO $pdo)
-
 {
-$this->pdo = $pdo;
+    $this->pdo = $pdo;
 }
 
-public function getOneById(int $id): int 
+public function getOneById(int $id): ServiceMapping
+
      {
-        $prepare = $this->pdo->prepare("SELECT * FROM `cmc_service` WHERE `serviceID` = :id");
-        $prepare->bindValue(":id", $id, PDO::PARAM_INT);
-        try {
-            $prepare->execute();
-            $result = $prepare->fetch();
+        $query = $this->pdo->prepare("SELECT * FROM `cmc_service` WHERE `serviceID` = :id");
+        $query->execute(['id' => $id]);
+        $service = $query->fetch();
+        if($service === false) {}
+        //t es la 
+        throw new \Exception("Aucun service ne coreespond à l'id $id
+        ")
+      /*  try {
+            $query->execute();
+            
             return new ServiceMapping($result);
         } catch (Exception $e) {
             echo "Erreur de requête : " . $e->getMessage();
             exit;
         }
     
-    }
+    }*/
 
     public function getAll(): ?array
     {
-        $prepare = $this->connect->prepare("SELECT * FROM cmc_service");
+        $query = $this->connect->prepare("SELECT * FROM cmc_service");
         try {
-            $prepare->execute();
-            $result = $prepare->fetchAll();
+            $query->execute();
+            $result = $query->fetchAll();
             $all = [];
             foreach ($result as $row) {
                 $all[] = new ServiceMapping($row);
