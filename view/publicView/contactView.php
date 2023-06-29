5effe6e66a23553702032cb6cf86edad099b383a
Jenniferboo <bugara.jennifer@gmail.com>
2023-06-29 11:57:18 +0200

    matin contact+partie css

--- view/publicView/contactView.php
+++ view/publicView/contactView.php
@@ -5,8 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
+    <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
     
+        <h1>Contact</h1>
+    <div id="infoContact">
+        <h2 id="info">informations</h2>
+        <div id="horaireContact">
+        <h3>Horaires</h3>
+        <p>Lundi au Vendredi 08h00 à 20h00</p>
+        <h3>adresse</h3>
+        <p>Avenue Harles de GAule 422,</p>
+        <p>Tanger, Maroc</p>
+        <h3>Transport</h3>
+        <p>Bus : 44, 255 et 275</p>
+        </div>
+            <div id="map">
+                <img src="../public/img/admin.png" alt="">
+                <p>"carte a faire!"</p>
+            </div>
+        <h2 id="partenaire">Nos partenaires</h2>
+    </div>       
 </body>
 </html>
--- view/publicView/serviceView.php
+++ view/publicView/serviceView.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
+<div id="globalService">
     <nav>
         <?php
        require_once "src/menu.php";
@@ -16,40 +17,44 @@
     <div class="banniere"></div>
     <h1>Nos Services</h1>
 
-    <h1>medecin</h1>
-<div class="serviceDoc">
-    <div class="docteur">
-        <img src="../public/img/logo.png" alt="">
-        <h2>Dr Blala</h2>
-        <p>fr/nl/ar</p>
-    </div>   
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, optio quae atque similique sed explicabo quo, assumenda quidem praesentium dignissimos eligendi accusamus aliquid, sequi veniam! Distinctio repellendus in tempore quis? Doloribus, beatae? Rerum voluptatibus maiores dolores expedita tempore odio fugiat eum commodi error! Minus illum eos aspernatur ea modi, aut tempora magnam esse quibusdam eum dolorum, fuga laboriosam soluta asperiores</p>
-</div>
-
-    <h1>radiologie</h1>
-<div class="serviceDoc">
-    <div class="docteur">
-        <img src="../public/img/logo.png" alt="">
-        <h2>Dr rara</h2>
-        <p>fr/nl/ar</p>
-    </div>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, optio quae atque similique sed explicabo quo, assumenda quidem praesentium dignissimos eligendi accusamus aliquid, sequi veniam! Distinctio repellendus in tempore quis? Doloribus, beatae? Rerum voluptatibus maiores dolores expedita tempore odio fugiat eum commodi error! Minus illum eos aspernatur ea modi, aut tempora magnam esse quibusdam eum dolorum, fuga laboriosam soluta asperiores</p>
-</div>
     
-    <h1>gynécologie</h1>
-<div class="serviceDoc">
-    <div class="docteur">
-        <img src="../public/img/logo.png" alt="">
-        <h2>Dr Gégé</h2>
-        <p>fr/nl/ar</p>
-    </div>
+    <div class="serviceDoc">
+        <div class="docteur">
+            <img src="../public/img/logo.png" alt="">
+            <h3>Dr Blala</h3>
+            <p>fr/nl/ar</p>
+        </div>   
+    <h2>Dentiste</h2>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, optio quae atque similique sed explicabo quo, assumenda quidem praesentium dignissimos eligendi accusamus aliquid, sequi veniam! Distinctio repellendus in tempore quis? Doloribus, beatae? Rerum voluptatibus maiores dolores expedita tempore odio fugiat eum commodi error! Minus illum eos aspernatur ea modi, aut tempora magnam esse quibusdam eum dolorum, fuga laboriosam soluta asperiores</p>
-</div>
+    </div>
+
+    
+    <div class="serviceDoc">
+        <div class="docteur">
+            <img src="../public/img/logo.png" alt="">
+            <h2>Dr rara</h2>
+            <p>fr/nl/ar</p>
+        </div>
+    <h1>radiologie</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, optio quae atque similique sed explicabo quo, assumenda quidem praesentium dignissimos eligendi accusamus aliquid, sequi veniam! Distinctio repellendus in tempore quis? Doloribus, beatae? Rerum voluptatibus maiores dolores expedita tempore odio fugiat eum commodi error! Minus illum eos aspernatur ea modi, aut tempora magnam esse quibusdam eum dolorum, fuga laboriosam soluta asperiores</p>
+    </div>
+    
+    
+    <div class="serviceDoc">
+        <div class="docteur">
+            <img src="../public/img/logo.png" alt="">
+            <h2>Dr Gégé</h2>
+            <p>fr/nl/ar</p>
+        </div>
+    <h1>gynécologie</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, optio quae atque similique sed explicabo quo, assumenda quidem praesentium dignissimos eligendi accusamus aliquid, sequi veniam! Distinctio repellendus in tempore quis? Doloribus, beatae? Rerum voluptatibus maiores dolores expedita tempore odio fugiat eum commodi error! Minus illum eos aspernatur ea modi, aut tempora magnam esse quibusdam eum dolorum, fuga laboriosam soluta asperiores</p>
+    </div>
 
     <footer>
         <?php
         require_once "src/foot.php";
         ?>
     </footer>
+</div>
 </body>
 </html>
