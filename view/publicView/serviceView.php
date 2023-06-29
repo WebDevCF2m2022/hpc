4e2a21cdc3045e7867d5a577a4aafb6537507186
Jenniferboo <bugara.jennifer@gmail.com>
2023-06-29 13:35:41 +0200

    caca

--- view/publicView/serviceView.php
+++ view/publicView/serviceView.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nos services</title>
     <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="../public/css/menuCSS.css">
 </head>
 <body>
 <div id="globalService">
