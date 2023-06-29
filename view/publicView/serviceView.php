9dae02aa7c4e4edba8825f89f94eef26e15e6c3f
Jenniferboo <bugara.jennifer@gmail.com>
2023-06-29 15:18:21 +0200

    finmatt

--- view/publicView/serviceView.php
+++ view/publicView/serviceView.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nos services</title>
-    <link rel="stylesheet" href="../public/css/style.css">
-    <link rel="stylesheet" href="../public/css/menuCSS.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/menuCSS.css">
 </head>
 <body>
 <div id="globalService">
@@ -16,23 +16,26 @@
         ?>
     </nav>
     <div class="banniere"></div>
-    <h1>Nos Services</h1>
-
-    
+    <h1 id="titi">Nos Services</h1>
+    <div class="carteService">
     <div class="serviceDoc">
         <div class="docteur">
-            <img src="../public/img/logo.png" alt="">
+            <img src="../public/img/logoMed.png" alt="">
             <h3>Dr Blala</h3>
             <p>fr/nl/ar</p>
-        </div>   
+        
+        </div>
+        <div class="serviceSeul">   
     <h2>Dentiste</h2>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, optio quae atque similique sed explicabo quo, assumenda quidem praesentium dignissimos eligendi accusamus aliquid, sequi veniam! Distinctio repellendus in tempore quis? Doloribus, beatae? Rerum voluptatibus maiores dolores expedita tempore odio fugiat eum commodi error! Minus illum eos aspernatur ea modi, aut tempora magnam esse quibusdam eum dolorum, fuga laboriosam soluta asperiores</p>
+        </div>
+    </div>
     </div>
 
     
     <div class="serviceDoc">
         <div class="docteur">
-            <img src="../public/img/logo.png" alt="">
+            <img src="../public/img/logoMed.png" alt="">
             <h2>Dr rara</h2>
             <p>fr/nl/ar</p>
         </div>
@@ -43,7 +46,7 @@
     
     <div class="serviceDoc">
         <div class="docteur">
-            <img src="../public/img/logo.png" alt="">
+            <img src="../public/img/logoMed.png" alt="">
             <h2>Dr Gégé</h2>
             <p>fr/nl/ar</p>
         </div>
