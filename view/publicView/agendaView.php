a446d101aa98d05d8ac3cfdbf97e369dcadf0ff2
Jenniferboo <bugara.jennifer@gmail.com>
2023-07-12 09:55:20 +0200

    oublie

--- view/publicView/agendaView.php
+++ view/publicView/agendaView.php
@@ -5,8 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/menuCSS.css">
 </head>
 <body>
+<?php
+       require_once "src/menu.php";
+        ?>
+
+
+<footer>
+        <?php
+        require_once "src/foot.php";
+        ?>
+    </footer>
+
+
     
 </body>
 </html>
